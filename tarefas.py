9bc1403c18eb34d3a8f4dd891ccec2a72f3bb409
Thiago Mohallem <thiagomohallem1@gmail.com>
2019-09-23 16:31:46 -0300

    Adição da classe tarefas.py, cada tarefa pertence a uma estória

---
+++ tarefas.py
@@ -0,0 +1,46 @@
+import banco
+
+class Tarefa(object):
+
+	def __init__(self, id_estoria, nome, descricao,done, id = None):
+	
+		if id is None:
+			self.id = banco.proxTarefaID
+		else:
+			self.id = id
+
+		self.id_estoria = id_estoria
+		self.nome = nome
+		self.descricao = descricao
+		self.done = done
+
+	def insertTarefaBanco(self):
+	 
+		try:
+
+			banco.insertTarefa(self.id_estoria, self.nome, self.descricao,self.done)
+			return True
+
+		except  Exception as e:
+			#raise e
+			return False
+
+	def updateTarefaBanco(self):
+
+		try:
+
+			banco.updateTarefa(self.id,self.id_estoria ,self.nome, self.descricao,self.done)
+			return True
+
+		except:
+			return False
+
+	def deleteTarefaBanco(self):
+
+		try:
+			
+			banco.deleteTarefa(self.id)
+
+			return True
+		except:
+			return False
