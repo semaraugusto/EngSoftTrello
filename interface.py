93b15953969dba1f4ef4a16ab9c6fae1eefda2bd
Tiago <tiagonegri237@gmail.com>
2019-09-24 19:50:36 -0300

    initial page were created with all buttons but no functions associated

--- interface.py
+++ interface.py
@@ -1,9 +1,9 @@
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import font  as tkfont
-from estorias import Estoria
+#from estorias import Estoria
 import random
-import banco
+#import banco
 
 class SampleApp(tk.Tk):
 
@@ -21,15 +21,59 @@ class SampleApp(tk.Tk):
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
-        #self.frames["InitialPage"] = InitialPage(container, self)
-        #self.frames["InitialPage"].grid(row=0, column=0, sticky="nsew")
+        self.frames["InitialPage"] = InitialPage(container, self)
+        self.frames["InitialPage"].grid(row=0, column=0, sticky="nsew")
 
-        #self.show_frame("InitialPage")
+        self.show_frame("InitialPage")
 
     def show_frame(self, page_name):
         '''Show a frame for the given page name'''
         frame = self.frames[page_name]
         frame.tkraise()
+
+
+class InitialPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        self.controller = controller
+        self.projects = ["AEDSII-TP1", "COMPILADORESI-TP2", "ENGSOFT-TP3", "OCII-TP0"]
+        self.initial_page_options = ["Create new project", "Delete project", "Configure project"]
+        self.fontePadrao = ("Arial", "10")
+
+
+        label = tk.Label(self, font=controller.title_font, pady=1)
+        label.pack(fill="x")
+
+        
+        for i in range(0,len(self.initial_page_options)):
+            brightness = 1
+            bg_colour = "#0A6450"
+            option = tk.Button(controller, text=self.initial_page_options[i], fg='White' if brightness < 120 else 'Black', bg=bg_colour)
+            option.place(x = 20, y = 30 + i*50, width=120, height=30)
+
+        self.projects_list_box = tk.Listbox(controller, width=70, height=35, selectmode=tk.BROWSE)
+        for i in range(0, len(self.projects)):
+            self.projects_list_box.insert(i, self.projects[i])
+        self.projects_list_box.place(x=200, y=30)
+
+        #banco.inicializaBanco()
+        #self.estorias = []
+        #self.carregaEstorias()
+
+    #def carregaEstorias(self):
+    #    command = "SELECT * FROM estorias;"
+    #    result = banco.executeQuery(command)
+
+    #    for linha in result:
+    #        self.estorias.append(Estoria(linha[1], linha[2], linha[3], linha[0]))
+
+
+    #def printEstorias(self):
+    #    for estoria in self.estorias:
+    #        print(str(estoria.id) + ", " + estoria.nome + ", " + estoria.descricao + ", " + str(estoria.story_points))
+
   
 app = SampleApp()
 app.geometry("800x600")
