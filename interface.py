b41fc0e9329e056a65fe4762d3d0000d2922bdea
Tiago <tiagonegri237@gmail.com>
2019-09-24 23:11:46 -0300

    widget passed in functions changed to corret some bugs

--- interface.py
+++ interface.py
@@ -5,77 +5,102 @@ from tkinter import font  as tkfont
 import random
 #import banco
 
-class SampleApp(tk.Tk):
+class Application(tk.Tk):
 
     def __init__(self, *args, **kwargs):
+        
         tk.Tk.__init__(self, *args, **kwargs)
-
-        self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
-
-        # the container is where we'll stack a bunch of frames
-        # on top of each other, then the one we want visible
-        # will be raised above the others
         container = tk.Frame(self)
-        container.pack(side="top", fill="both", expand=True)
+
+        container.pack(side="top", fill="both", expand = True)
+
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
-        self.frames["InitialPage"] = InitialPage(container, self)
-        self.frames["InitialPage"].grid(row=0, column=0, sticky="nsew")
 
-        self.show_frame("InitialPage")
+        for F in [InitialPage]:#, ProjectWindow, PageTwo):
 
-    def show_frame(self, page_name):
-        '''Show a frame for the given page name'''
-        frame = self.frames[page_name]
+            frame = F(container, self)
+
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(InitialPage)
+
+    def show_frame(self, cont):
+
+        frame = self.frames[cont]
+        frame.tkraise()
+
+    def show_projectFrame(self, project_name):
+        print(project_name)
+        frame = self.frames[InitialPage]
+        frame.defineProjectName(project_name)
         frame.tkraise()
 
 
 class InitialPage(tk.Frame):
 
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
+        tk.Frame.__init__(self,parent)
 
-        self.controller = controller
+        # Initializing Main label in the frame
+        label = tk.Label(self, width=800, height=600)
+        label.pack(pady=10,padx=10)
+
+        # getting the projects avaiable
         self.projects = ["AEDSII-TP1", "COMPILADORESI-TP2", "ENGSOFT-TP3", "OCII-TP0"]
 
-        self.initial_page_options = ["Create new project", "Delete project", "Configure project"]
-        self.fontePadrao = ("Arial", "10")
-
-        label = tk.Label(self, font=controller.title_font, pady=1)
-        label.pack(fill="x")
-
+        # options visual configurations
         self.options_brightness = "White"
         self.options_colour = "#0A6450"
-        self.options_count = 0        
+        self.options_count = 0
 
-        self.createOption(self.controller, "Create new project", self.createNewProject)
-        self.createOption(self.controller, "Delete project", self.deleteProject)
+        # creating options avaiable
+        self.createOption(self, "Create new project", self.createNewProject)
+        self.createOption(self, "Delete project", self.deleteProject)
 
-        self.projects_list_box = tk.Listbox(controller, width=70, height=35, selectmode=tk.BROWSE)
+        # creating the list of projects that the user have
+        self.projects_list_box = tk.Listbox(self, width=70, height=35, selectmode=tk.BROWSE)
         for i in range(0, len(self.projects)):
             self.projects_list_box.insert(i, self.projects[i])
+        
+        self.projects_list_box.bind("<Double-Button-1>", lambda x: controller.show_projectFrame(self.projects_list_box.get(tk.ACTIVE)))
         self.projects_list_box.place(x=200, y=30)
 
+        #button2 = tk.Button(self, text="Visit Page 2", command=lambda: controller.show_frame(PageTwo))
+        #button2.place()
+
+    # function that create the buttons options
     def createOption(self, widget, option_string, function):
         option = tk.Button(widget, text=option_string, command=function, fg=self.options_brightness , bg=self.options_colour)
         option.place(x = 20, y = 30 + self.options_count*50, width=120, height=30)
         self.options_count += 1
 
+    # get the new project data given by the user, and 
     def getSubmited(self, widget, name_entry, description_entry):
         name = name_entry.get()
         description = description_entry.get()
 
         if name != "" and description != "":
-            #addprojectfunction()
+            # addProjectInDataBase()
+            # updateInterfaceData()
             name_entry.destroy()
             description_entry.destroy()
             widget.destroy()
         else:
+            # popup an error message and keeps the window open
             Errorlabel = tk.Label(widget, text="Name or description not given", background="red", fg="white")
             Errorlabel.grid(row=2,column=0)
 
+    def cancelSubmited(self, widget, name_entry, description_entry):
+        name_entry.destroy()
+        description_entry.destroy()
+        widget.destroy()
+
+    # Create the new project window popup to get the new project data
     def createNewProject(self):
         win = tk.Toplevel()
         win.wm_title("Window")
@@ -94,24 +119,29 @@ class InitialPage(tk.Frame):
         submit_button = tk.Button(win, text="Submit", command= lambda: self.getSubmited(win, name_entry, description_entry))
         submit_button.grid(row=2, column=1)
 
+        submit_button = tk.Button(win, text="Cancel", command= lambda: self.cancelSubmited(win, name_entry, description_entry))
+        submit_button.grid(row=2, column=2)
+
     def deleteYesButton(self, widget, project_selected):
-        #delete project function
+        # deleteProjectInDataBase()
+        # updateInterfaceData()
         widget.destroy()
 
     def deleteNoButton(self, widget):
-        #delete project function
         widget.destroy()
 
+    # Create the delete project window popup
     def deleteProject(self):
         win = tk.Toplevel()
         win.wm_title("Window")
 
         project_selected = self.projects_list_box.get(tk.ACTIVE)
-        label= tk.Label(win, text="Are you sure you want to delete '{}'?".format(project_selected),font=10)
+        label= tk.Label(win, text="Are you sure you want to delete '{}' ?".format(project_selected),font=10)
         label.grid(row=0)
 
         yes_button = tk.Button(win, text="yes", command= lambda: self.deleteYesButton(win, project_selected))
         yes_button.grid(row=4, column=1)
+
         no_button = tk.Button(win, text="no", command= lambda: self.deleteNoButton(win))
         no_button.grid(row=4, column=2)
         
@@ -131,7 +161,6 @@ class InitialPage(tk.Frame):
     #    for estoria in self.estorias:
     #        print(str(estoria.id) + ", " + estoria.nome + ", " + estoria.descricao + ", " + str(estoria.story_points))
 
-  
-app = SampleApp()
+app = Application()
 app.geometry("800x600")
 app.mainloop()
